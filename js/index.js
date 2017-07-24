264710a029f11a381763d32e0b97b3cc2b71e0c6
xs <xs@xsdeMacBook-Pro.local>
2017-07-24 10:22:36 +0800

    建模完毕？

--- js/index.js
+++ js/index.js
@@ -15,10 +15,10 @@ var i=0;
 var background = "#FFE4C4"
 var c=document.getElementById("myCanvas");
 var ctx=c.getContext("2d");
-var BACK_WIDTH = 960;   //画布尺寸宽度
-var BACK_HEIGHT = 720;   //画布尺寸高度
-var UNIT_WIDTH= 24; //每个单元（图标）宽度
-var UNIT_HEIGHT= 24; //每个单元（图标）高度
+var BACK_WIDTH = 960;   // 画布尺寸宽度
+var BACK_HEIGHT = 720;   // 画布尺寸高度
+var UNIT_WIDTH= 24; // 每个单元（图标）宽度
+var UNIT_HEIGHT= 24; // 每个单元（图标）高度
 var P_BODY = document.getElementById("python_body")
 var P_FACE1 = document.getElementById("python_face1")
 var APPLE = document.getElementById("icon_apple")
@@ -27,6 +27,7 @@ var WATERMELON = document.getElementById("icon_watermelon")
 var STRAWBERRY = document.getElementById("icon_strawberry")
 var BANANA = document.getElementById("icon_banana")
 var PEAR = document.getElementById("icon_pear")
+var startFlag = 0;
 var FOOD_LIST = [
     {food : APPLE},
     {food : ORANGE},
@@ -35,6 +36,17 @@ var FOOD_LIST = [
     {food : BANANA},
     {food : PEAR},
     ];
+var DIRECTION = {
+    up :{x:0, y:-1},
+    down :{x:0, y:1},
+    right :{x:1, y:0},
+    left :{x:-1, y:0},
+};
+var CODE_DIRECTION_MAP = [];
+CODE_DIRECTION_MAP[269] = "up";
+CODE_DIRECTION_MAP[270] = "down";
+CODE_DIRECTION_MAP[271] = "left";
+CODE_DIRECTION_MAP[272] = "right";
 /**
  * 定义贪吃蛇对象
  * @type {{}}
@@ -43,17 +55,16 @@ var pythonList = [];    //贪吃蛇定义一个数组来维护
 var direction = 0;      //0-->往左；1-->往上；2-->往右；3-->往下
 var lastTime = Date.now();  //用来控制速度
 var deltaTime = 0;
+var python = new Python();
 var init = function() {
     ctx.fillStyle=background;
     ctx.fillRect(0,0,BACK_WIDTH,BACK_HEIGHT);
-    python.init();
-    var x = 0;var y=BACK_HEIGHT/2;
-    ctx.drawImage(P_BODY, x, y, UNIT_WIDTH, UNIT_HEIGHT);
-    x += 24;
-    ctx.drawImage(P_BODY, x, y, UNIT_WIDTH, UNIT_HEIGHT);
-    x += 24;
-    ctx.drawImage(P_FACE1, x, y, UNIT_WIDTH, UNIT_HEIGHT);
-    for (var i=0; i<3; i++) {
+    python.init(3, 0, BACK_HEIGHT/2, P_FACE1, P_BODY, UNIT_WIDTH, UNIT_HEIGHT);
+    for (var i=0; i<python.bodyList.length; i++) {
+        ctx.drawImage(python.bodyList[i].img, python.bodyList[i].x, python.bodyList[i].y, UNIT_WIDTH, UNIT_HEIGHT);
+    }
+    ctx.drawImage(python.head.img, python.head.x, python.head.y, UNIT_WIDTH, UNIT_HEIGHT);
+    for (var i=0; i<10; i++) {
         x = Math.random() * (BACK_WIDTH - UNIT_WIDTH);
         y = Math.random() * (BACK_HEIGHT - UNIT_HEIGHT);
         var foodIndex = parseInt(Math.random() * FOOD_LIST.length);
@@ -62,18 +73,48 @@ var init = function() {
         }
     }
 }
-$(function() {
-    init();
-    requestAnimationFrame(gameLoop);
-})
-
-
 var refreshDeltaTime = function () {
     var now = Date.now();
     deltaTime = now - lastTime;
     lastTime = now;
 };
-var gameLoop = function() {
-    refreshDeltaTime();
-    requestAnimationFrame(gameLoop());
+var i=0;
+function gameLoop() {
+    if (startFlag) {
+        refreshDeltaTime();
+        if (i++ < 100) requestAnimationFrame(gameLoop);
+        else startFlag = !startFlag;
+        console.log(i + "...");
+    }
 }
+var onKeyPress = function(code) {
+    switch(code) {
+        case 13 :
+            // 开始/暂停
+            i=0;
+            startFlag = !startFlag;
+            if (startFlag) {
+                requestAnimationFrame(gameLoop);
+            }
+            break
+        case 269 :
+            // up
+            break;
+        case 270 :
+            // down
+        case 271 :
+            // left
+        case 272 :
+            // right
+    }
+}
+
+$(function() {
+    init();
+    $("body").bind("keydown", function(e) {
+        // 兼容FF和IE和Opera
+        var theEvent = e || window.event;
+        var code = theEvent.keyCode || theEvent.which || theEvent.charCode;
+        onKeyPress(code);
+    })
+})
--- js/pythonObj.js
+++ js/pythonObj.js
@@ -2,12 +2,24 @@
  * 贪吃蛇对象
  * @type {{}}
  */
+function Python() {
+}
 function Python(head, bodyList) {
     this.head = head;
     this.bodyList = bodyList;
-    this.size = 1 + bodyList.length;
+    if (bodyList)
+        this.size = head?1:0 + bodyList.length;
+    else
+        this.size = head?1:0;
+    this.direction = 2;
 }
-
+/**
+ * 贪吃蛇每一节
+ * @param x
+ * @param y
+ * @param img
+ * @constructor
+ */
 function PythonUnit(x, y, img) {
     // 绝对定位
     this.x = x;
@@ -22,5 +34,16 @@ function PythonUnit(x, y, img) {
  */
 Python.prototype.init = function(length, tailX, tailY, headImg, bodyImg, imgWidth, imgHeight) {
     if (!length) length = 2;
-
+    var x = tailX; var y=tailY;
+    var bodyList = [];
+    for (var i=0; i< length; i++) {
+        var bodyUnit = new PythonUnit(x,y, bodyImg);
+        bodyList.push(bodyUnit);
+        x += imgWidth;
+    }
+    var head = new PythonUnit(x,y, headImg);
+    this.head = head;
+    this.bodyList = bodyList;
+    this.size = 1 + bodyList.length;
+    this.direction = 2;
 }
